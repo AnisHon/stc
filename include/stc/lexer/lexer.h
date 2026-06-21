915ed70de88f44c3b777ebf5d440496cae91567d
anishan <3137687133@qq.com>
2026-06-21 23:52:00 +0800

    WIP：需要实现comment处理逻辑，为了处理统一性，将comment处理为一个token，这样方便做错误处理

--- include/stc/lexer/lexer.h
+++ include/stc/lexer/lexer.h
@@ -20,11 +20,8 @@ class Lexer {
      * Lexer是一个状态机，这是状态
      */
     enum class State: std::uint8_t {
-        /// 初始状态
-        Init,
         /// 可能是字符串或标识符
         MaybeKeywordOrIdent,
-        MaybeComment,
         /// 可能是宏
         MaybeMacro,
         /// 错误状态，指针不会推进，只会返回 Invalid Token
@@ -33,8 +30,24 @@ class Lexer {
         Eof,
     };
 
+    enum class CommentKind: std::uint8_t {
+        NotComment,
+        SingleLineComment,
+        MultiLineComment,
+    };
+
+
+    /**
+     * 判断是否是新行
+     */
     [[nodiscard]]
-    bool is_comment_() const;
+    bool is_new_line() const;
+
+    [[nodiscard]]
+    CommentKind get_comment_type_() const;
+
+    [[nodiscard]]
+    bool is_comment_end_(CommentKind kind) const;
 
     [[nodiscard]]
     std::u8string_view get_current_view_() const;
@@ -45,7 +58,7 @@ class Lexer {
      * @return 取决于读到的第一个字符
      */
     [[nodiscard]]
-    State handle_init_state_() const;
+    State peek_state() const;
 
     /**
      * 如果 is_ident_start 成立就是则可以调用这个函数，否则出错
@@ -53,15 +66,38 @@ class Lexer {
     Token handle_keyword_or_ident_();
 
     /**
+     * 宏处理函数
+     */
+    Token handle_macro_();
+
+    /**
      * 移动迭代器，计算一个utf8码点，维护一个索引位置，不允许对已经结束的迭代器操作
      * @return 返回码点
      */
     char32_t next_();
 
+    /**
+     * peek 当前字符，当迭代器结束返回 zero 字符
+     */
     [[nodiscard]]
     char32_t peek_() const;
 
+    /**
+     * peek 两个字符，当迭代器结束返回 zero 字符
+     */
+    [[nodiscard]]
+    std::array<char32_t, 2> peek2_() const;
+
+    /**
+     * 跳过空白字符
+     */
     void skip_white_space();
+
+    /**
+     * 跳过注释处理函数
+     */
+    void skip_comment_();
+
 
     /**
      * @return 当前位置的loc
@@ -81,7 +117,7 @@ class Lexer {
     Token make_token_(TokenKind kind);
 
 public:
-    Lexer(source::Location start_offset, std::u8string_view source);
+    explicit Lexer(std::u8string_view source);
 
     /**
      * 获取下一个token，可能出现 EOF InvalidToken，
@@ -93,12 +129,6 @@ public:
 private:
     /// 是否是行开始状态
     bool is_start_of_line;
-
-    /// 状态机当前状态
-    State current_state_;
-
-    /// 索引开始的偏移量
-    const source::Location start_offset_;
 
     /// 每次 Token 结束的下一个位置
     std::u8string_view::const_iterator prev_iter_;
--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -26,11 +26,8 @@ static bool is_ident_start(const char32_t code_point) {
 
 namespace stc::lexer {
 Lexer::Lexer(
-    const source::Location start_offset,
     const std::u8string_view source
     ) : is_start_of_line(true)
-        , current_state_{State::Init}
-        , start_offset_{start_offset}
         , prev_iter_{source.cbegin()}
         , source_beg_{source.cbegin()}
         , current_iter_{source.cbegin()}
@@ -38,32 +35,64 @@ Lexer::Lexer(
     ASSERT(utf8::is_valid(source.begin(), source.end()), "无效UTF8字符串");
 }
 
+[[nodiscard]]
+bool Lexer::is_new_line() const {
+    const auto arr = peek2_();
+
+    const bool is_lf = arr[0] == U'\n';
+    const bool is_cr = arr[0] == U'\r';
+    const bool is_crlf = arr[0] == U'\r' && arr[1] == U'\n';
+
+    return is_lf || is_cr || is_crlf;
+}
+
 /**
  * 直接使用迭代器判断是否是注释
  */
-bool Lexer::is_comment_() const {
-    const char8_t curr{*this->current_iter_};
-    const char8_t next{*(this->current_iter_ + 1)};
+Lexer::CommentKind Lexer::get_comment_type_() const {
+    // 当前迭代器已经结束了一定不是 comment
+    const auto arr = this->peek2_();
 
-    const bool is_comment_start{curr == u8'/'};
-    const bool is_comment_next{next == u8'/' || next == u8'*'};
+    if (arr[0] == u8'/' && arr[1] == u8'/') {
+        return CommentKind::SingleLineComment;
+    }
+    if (arr[0] == u8'/' || arr[1] == u8'/') {
+        return CommentKind::MultiLineComment;
+    }
+    return CommentKind::NotComment;
+}
 
-    return is_comment_start && is_comment_next;
+bool Lexer::is_comment_end_(const CommentKind kind) const {
+    ASSERT(kind != CommentKind::NotComment);
+
+    const std::array<char32_t, 2> arr = this->peek2_();
+
+    switch (kind) {
+    case CommentKind::SingleLineComment: {
+        const bool new_line = this->is_new_line();
+        return arr[0] == U'\0' || new_line; // 换行或者结束
+    }
+    case CommentKind::MultiLineComment:
+        return arr[0] == u8'*' && arr[1] == u8'/'; // 不用多说，很正常
+    default:
+        UNREACHABLE("到达不可能注释分支");
+    }
 }
 
 /**
  * 获取当前区间的 string_view_()
  */
 std::u8string_view Lexer::get_current_view_() const {
-    const std::u8string_view curr(this->source_beg_, this->source_end_);
-    return curr; // 应该没问题
+    return {this->source_beg_, this->source_end_};
 }
 
 /**
  * 下一个码点，移动迭代器
  */
 inline char32_t Lexer::next_() {
-    ASSERT(this->current_iter_ != this->source_end_);
+    if (this->current_iter_ == this->source_end_) {
+        return U'\0';
+    }
     const auto chr = utf8::next(this->current_iter_, source_end_);
 
     // 维护状态
@@ -76,41 +105,62 @@ inline char32_t Lexer::next_() {
     return chr;
 }
 
+[[nodiscard]]
 inline char32_t Lexer::peek_() const {
-    ASSERT(this->current_iter_ != this->source_end_);
+    if (this->current_iter_ == this->source_end_) {
+        return U'\0';
+    }
     return utf8::peek_next(this->current_iter_, source_end_);
 }
 
-void Lexer::skip_white_space() {
-    while (utils::is_ascii_whitespace(this->peek_())) {
-        this->next_();
+/**
+ * peek 两个字符，当迭代器结束返回 zero 字符
+ */
+[[nodiscard]]
+inline std::array<char32_t, 2> Lexer::peek2_() const {
+    std::array res = {U'\0', U'\0'};
+    auto it = current_iter_;
+    if (it != source_end_) {
+        res[0] = utf8::next(it, source_end_);
     }
-    this->prev_iter_ = this->current_iter_;
+
+    if (it != source_end_) {
+        res[1] = utf8::peek_next(it, source_end_);
+    }
+
+    return res;
 }
 
 /**
  * init 状态下的状态转移
  * 这个函数保证不会移动指针，不会修改任何内容
  */
-Lexer::State Lexer::handle_init_state_() const {
+Lexer::State Lexer::peek_state() const {
     // init_state 下应该没有结束但是没有使用的情况
-    if (this->current_iter_ == this->source_end_) {
+
+    const char32_t chr{this->peek_()};
+
+    // 注释
+    if (this->get_comment_type_() != CommentKind::NotComment) {
+        return State::MaybeComment;
+    }
+
+    // 关键字或标识符
+    if (is_ident_start(chr)) {
+        return State::MaybeKeywordOrIdent;
+    }
+
+    // 宏
+    if (chr == U'#' && this->is_start_of_line) {
+        return State::MaybeMacro;
+    }
+
+    // 结束
+    if (chr == U'\0') {
         return State::Eof;
     }
 
-    auto current_state{State::Invalid};
-    const char32_t chr{this->peek_()};
-    if (this->is_comment_()) {
-        current_state = State::MaybeComment;
-    } else if (is_ident_start(chr)) {
-        current_state = State::MaybeKeywordOrIdent;
-    } else if (chr == U'#' && this->is_start_of_line) {
-        current_state = State::MaybeMacro;
-    } else {
-        TODO("todo", "需要实现 宏 运算符 常量 注释 等"); // todo
-    }
-
-    return current_state;
+    TODO("todo", "需要实现 运算符 常量 等"); // todo
 }
 
 /**
@@ -130,33 +180,72 @@ Token Lexer::handle_keyword_or_ident_() {
     return this->make_token_(kind);
 }
 
+Token Lexer::handle_macro_() {
+    TODO("宏处理未实现");
+}
+
+/**
+ * 跳过空白字符
+ */
+void Lexer::skip_white_space() {
+    while (utils::is_ascii_whitespace(this->peek_())) {
+        this->next_();
+    }
+    this->prev_iter_ = this->current_iter_;
+}
+
+/**
+ * 跳过注释
+ */
+void Lexer::skip_comment_() {
+    const auto comment_type = this->get_comment_type_();
+    if (comment_type == CommentKind::NotComment) {
+        // 不是注释跳过
+        return;
+    }
+
+    bool reach_end = false;
+
+    while (this->current_iter_ != this->source_end_) {
+        if (this->is_comment_end_(comment_type)) {
+            // 到达 comment 结束退出
+            reach_end = true;
+            break;
+        }
+    }
+
+    TODO("实现Comment处理逻辑")
+    //if (reach_end) {
+    //    return make_token_(TokenKind::)
+    //} else {
+    //    return invalid_token();
+    //}
+}
+
+
 /**
  * 返回当前位置location
  */
+[[nodiscard]]
 source::Location Lexer::make_current_location_() const {
     const auto sz{static_cast<std::uint32_t>(this->current_iter_ - this->source_beg_)};
-    return start_offset_ + sz;
+    return {sz};
 }
 
 /**
  * 返回当前的range
  */
+[[nodiscard]]
 source::LocationRange Lexer::make_range_() const {
     const auto curr_sz{static_cast<std::uint32_t>(this->current_iter_ - this->source_beg_)};
     const auto prev_sz{static_cast<std::uint32_t>(this->prev_iter_ - this->source_beg_)};
 
     // 当前token
-    const source::Location curr{
-        this->start_offset_ + curr_sz
-    };
+    const source::Location curr{curr_sz};
 
-    const source::Location prev{
-        this->start_offset_ + prev_sz
-    };
+    const source::Location prev{prev_sz};
 
-    const source::LocationRange range{
-        prev, curr
-    };
+    const source::LocationRange range{prev, curr};
 
     return range;
 }
@@ -173,24 +262,23 @@ Token Lexer::make_token_(const TokenKind kind) {
 
 Token Lexer::next_token() {
 
-    for (;;) {
-        // 跳过空白字符
-        this->skip_white_space();
-        switch (this->current_state_) {
-        case State::Init: // 状态转换
-            this->current_state_ = handle_init_state_();
-            break;
-        case State::Invalid: // 不推进，没有状态转移
-            return invalid_token();
-        case State::Eof: // lexer 已经结束，返回 EOF
-            return Token{TokenKind::Eof, null_lexeme(), make_range_()};
-        case State::MaybeKeywordOrIdent: // 解析
-            return this->handle_keyword_or_ident_();
-        default:
-            TODO();
-        }
-
+    // 跳过空白字符
+    this->skip_white_space();
+    this->skip_comment_();
+    const auto current_state = peek_state();
+    switch (current_state) {
+    case State::MaybeKeywordOrIdent: // 解析
+        return this->handle_keyword_or_ident_();
+    case State::MaybeMacro:
+        return this->handle_macro_();
+    case State::Invalid: // 不推进，没有状态转移
+        return invalid_token();
+    case State::Eof: // lexer 已经结束，返回 EOF
+        return Token{TokenKind::Eof, null_lexeme(), make_range_()};
+    default:
+        TODO();
     }
+
     UNREACHABLE("代码错误，跳出死循环");
 }
 
