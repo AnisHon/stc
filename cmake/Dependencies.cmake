e64605e3b94db25ac2503ff0ceb1afe078c27985
anishan <3137687133@qq.com>
2026-07-14 23:36:58 +0800

    修改cmake 更新依赖

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,12 +1,14 @@
 include(FetchContent)
 
-# Dependencies:  fmt cpptrace spdlog utf8cpp catch2
+# Dependencies:  fmt cpptrace utf8cpp catch2
 
-find_package(fmt CONFIG QUIET)
+if (STC_USE_SYSTEM_DEPENDENCIES)
+    find_package(fmt 12.1.0 CONFIG QUIET)
+endif ()
 if (fmt_FOUND)
     message(STATUS "Found system fmt ${fmt_VERSION}")
 else ()
-    message(STATUS "System fmt not found, fetching v12.1.0")
+    message(STATUS "Using fetched fmt v12.1.0")
     FetchContent_Declare(fmt
             GIT_REPOSITORY https://github.com/fmtlib/fmt.git
             GIT_TAG 12.1.0
@@ -14,11 +16,13 @@ else ()
     FetchContent_MakeAvailable(fmt)
 endif ()
 
-find_package(cpptrace CONFIG QUIET)
+if (STC_USE_SYSTEM_DEPENDENCIES)
+    find_package(cpptrace 1.0.4 CONFIG QUIET)
+endif ()
 if (cpptrace_FOUND)
     message(STATUS "Found system cpptrace ${cpptrace_VERSION}")
 else ()
-    message(STATUS "System cpptrace not found, fetching v1.0.4")
+    message(STATUS "Using fetched cpptrace v1.0.4")
     FetchContent_Declare(cpptrace
             GIT_REPOSITORY https://github.com/jeremy-rifkin/cpptrace.git
             GIT_TAG v1.0.4
@@ -26,48 +30,39 @@ else ()
     FetchContent_MakeAvailable(cpptrace)
 endif ()
 
-find_package(spdlog CONFIG QUIET)
-if (spdlog_FOUND)
-    message(STATUS "Found system spdlog ${spdlog_VERSION}")
-else ()
-    message(STATUS "System spdlog not found, fetching v1.17.0")
-    FetchContent_Declare(spdlog
-            GIT_REPOSITORY https://github.com/gabime/spdlog.git
-            GIT_TAG v1.17.0
-    )
-    FetchContent_MakeAvailable(spdlog)
+if (STC_USE_SYSTEM_DEPENDENCIES)
+    find_package(utf8cpp 4.1.1 CONFIG QUIET)
 endif ()
-
-find_package(utf8cpp CONFIG QUIET)
 if (utf8cpp_FOUND)
     message(STATUS "Found system utf8cpp ${utf8cpp_VERSION}")
 else ()
-    message(STATUS "System utf8cpp not found, fetching v4.1.1")
+    message(STATUS "Using fetched utf8cpp v4.1.1")
     FetchContent_Declare(utf8cpp
             GIT_REPOSITORY https://github.com/nemtrif/utfcpp.git
             GIT_TAG v4.1.1
     )
     FetchContent_MakeAvailable(utf8cpp)
+    add_library(utf8cpp::utf8cpp ALIAS utf8cpp)
 endif ()
 
 if (BUILD_TESTING)
-    find_package(Catch2 3 CONFIG QUIET)
+    if (STC_USE_SYSTEM_DEPENDENCIES)
+        find_package(Catch2 3.15.0 CONFIG QUIET)
+    endif ()
     if (Catch2_FOUND)
         message(STATUS "Found system Catch2 ${Catch2_VERSION}")
     else ()
-        message(STATUS "System Catch2 3 not found, fetching v3.15.0")
+        message(STATUS "Using fetched Catch2 v3.15.0")
         FetchContent_Declare(Catch2
-            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-            GIT_TAG v3.15.0
+                GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+                GIT_TAG v3.15.0
         )
         FetchContent_MakeAvailable(Catch2)
     endif ()
 endif ()
 
-add_library(stc_dependencies INTERFACE)
-target_link_libraries(stc_dependencies INTERFACE
+add_library(stc_common_deps INTERFACE)
+target_link_libraries(stc_common_deps INTERFACE
         fmt::fmt
-        utf8cpp::utf8cpp
-        spdlog::spdlog
         cpptrace::cpptrace
 )
